217559c577ce875ddeebcdb3bbf747c5e03ed5bc
yoshizuki <yoshizukiyo@gmail.com>
2022-05-30 00:10:15 +0900

    테이블 ajax 데이터 연동 및 부트스트랩 자료 table jqGrid 라이브러리

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 제이쿼리 플러그인
 https://jquery.malsup.com/
 ajax에서 form 태그에 관련된 플러그인도 있다
+http://www.trirand.com/blog/ 제이쿼리 그리드 플러그인
---
+++ study/resource/js/Guriddo_jqGrid_JS_demo/jqgridjs/loading_data/background/jq-config.php
@@ -0,0 +1,15 @@
+<?php
+// ** MySQL settings ** //
+//define('DB_NAME', 'northwind');    // The name of the database
+//define('DB_HOST', 'localhost');    // 99% chance you won't need to change this value
+define('DB_DSN','mysql:host=localhost;dbname=guriddon_northwind');
+define('DB_DSN1','mysql:host=localhost;dbname=guriddon_misim');
+define('DB_USER', 'guriddon_tony');     // Your MySQL username
+define('DB_PASSWORD', 'guri123ddo'); // ...and password
+define('DB_DATABASE', 'guriddon_northwind'); // ...and password
+
+define('ABSPATH', '../../../');
+// Form settings
+$SERVER_HOST = "";        // the host name
+$SELF_PATH = "";    // the web path to the project without http
+$CODE_PATH = "../../../../php/PHPSuito/"; // the physical path to the php files
